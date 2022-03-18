2ba593f83b8a3d72c71a6ad94c470684ca148d9e
Gagan Deep <61630537+Gagandeep-22@users.noreply.github.com>
2022-03-18 20:22:18 +0530

    accessing backend

--- components/DownloadFiles.js
+++ components/DownloadFiles.js
@@ -3,36 +3,41 @@ import './DownloadFile.css'
 
 const DownloadFile = () => {
 
-const pdfFiles = [
-    {
-      title: "pdf1.pdf",
-    },
-    {
-      title: "pdf2.pdf",
-    },
-    {
-      title: "pdf3.pdf",
-    },
-    {
-      title: "pdf4.pdf",
-    },
-    {
-      title: "pdf5.pdf",
-    },
-  ];
-  const renderFiles = (File, index) => {
+  const pdfList = () => {
+    fetch('http://localhost:8080/employees/download')
+      .then(response => response.blob()
+      .then(showFile(response)));
+  }
+
+  function showFile(fileAvailable) {
+    for (let i = 0; i < fileAvailable.length; i++) {
+      var filename = fileAvailable[i].filename;  
+      renderFiles(filename);
+    }
+  }
+  const renderFiles = (props) => {
     return (
-        <div key={index}>{File.title}</div>
+        <div>{props.name}</div>
     );
   };
 const ClickHandle = () => {
     console.log('Button Clicked')
+    fetch('http://localhost:8080/employees/download')
+			.then(response => {
+				response.blob().then(blob => {
+					let url = window.URL.createObjectURL(blob);
+					let a = document.createElement('a');
+					a.href = url;
+					a.download = 'employees.json';
+					a.click();
+				});
+		});
 }
   return (
     <div className='DownloadSection'>
         <div className='fileList'>
             <div>
-                {pdfFiles.map(renderFiles)}
+                {pdfList}
             </div>
         </div>
         <button onClick={ClickHandle} type="button" className="btn btn-primary btn-sm">Download</button>
@@ -41,3 +46,49 @@ const ClickHandle = () => {
 }
 
 export default DownloadFile
+
+
+
+// import React from 'react'
+// import './DownloadFile.css'
+
+// const DownloadFile = () => {
+
+// const pdfFiles = [
+//     {
+//       title: "pdf1.pdf",
+//     },
+//     {
+//       title: "pdf2.pdf",
+//     },
+//     {
+//       title: "pdf3.pdf",
+//     },
+//     {
+//       title: "pdf4.pdf",
+//     },
+//     {
+//       title: "pdf5.pdf",
+//     },
+//   ];
+//   const renderFiles = (File, index) => {
+//     return (
+//         <div key={index}>{File.title}</div>
+//     );
+//   };
+// const ClickHandle = () => {
+//     console.log('Button Clicked')
+// }
+//   return (
+//     <div className='DownloadSection'>
+//         <div className='fileList'>
+//             <div>
+//                 {pdfFiles.map(renderFiles)}
+//             </div>
+//         </div>
+//         <button onClick={ClickHandle} type="button" className="btn btn-primary btn-sm">Download</button>
+//     </div>
+//   )
+// }
+
+// export default DownloadFile
